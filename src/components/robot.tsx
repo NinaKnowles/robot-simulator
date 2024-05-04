f9ac6f59357a08108cab58580082af57a21006d5
nina.knowles <nina.knowles@paconsulting.com>
2024-05-04 09:17:08 +0100

    Create robot avatar

---
+++ src/components/robot.tsx
@@ -0,0 +1,16 @@
+const Robot = () => {
+
+    return (
+        <div className="robot">
+            <div className="antenna"></div>
+            <div className="head"></div>
+            <div className="torso">
+                <div className="left">j</div>
+                <div className="right">j</div>
+            </div>
+            <div className="foot"></div>
+        </div>
+    )
+};
+
+export default Robot;
